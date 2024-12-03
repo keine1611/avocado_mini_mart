2a00c1552b1d391001141298750ec8fb21f79f2b
Kei <keiit1611@gmail.com>
2024-12-04 06:05:15 +0700

    6h05

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ const app = express()
 const server = http.createServer(app)
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
