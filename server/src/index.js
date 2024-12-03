56a9a551438c520ad659ce82044838ae5e5e0734
Kei <keiit1611@gmail.com>
2024-12-04 04:24:12 +0700

    4h24

--- server/src/index.js
+++ server/src/index.js
@@ -17,14 +17,14 @@ const server = http.createServer(app)
 const corsOptions = {
   origin: process.env.CLIENT_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: '*',
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }
-
+app.use(cors(corsOptions))
+app.options('*', cors(corsOptions))
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors(corsOptions))
 
 connectToDB()
 // syncDatabase()
