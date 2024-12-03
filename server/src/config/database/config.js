7eeb77a6208da195290edf33839dfc3135346b8d
Kei <keiit1611@gmail.com>
2024-12-03 20:55:14 +0700

    env db config

--- server/src/config/database/config.js
+++ server/src/config/database/config.js
@@ -1,8 +1,14 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = process.env
+
 export const config = {
-  HOST: 'localhost',
-  USER: 'root',
-  PASSWORD: '1234',
-  DB: 'minimart',
+  HOST: DB_HOST,
+  USER: DB_USER,
+  PASSWORD: DB_PASSWORD,
+  DB: DB_NAME,
   DIALECT: 'mysql',
-  PORT: 3306,
+  PORT: DB_PORT,
 }
