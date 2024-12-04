0e4a2fe834a9f950d4c6099a2b964aa99c3ecdc4
Kei <keiit1611@gmail.com>
2024-12-05 05:01:29 +0700

    add domain

--- server/src/utils/jwt.js
+++ server/src/utils/jwt.js
@@ -34,7 +34,8 @@ export const setTokenCookie = ({
       : expiresIn
       ? expiresIn * 60 * 1000
       : 30 * 24 * 60 * 60 * 1000,
-    secure: process.env.PRODUCTION == 'true' ? true : false,
+    secure: process.env.PRODUCTION == 'true' ? 'true' : 'false',
+    domain: process.env.COOKIE_DOMAIN,
   })
 }
 
