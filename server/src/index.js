a132602dce6a9b47a41748963ee92ada5cfd3194
Kei <keiit1611@gmail.com>
2024-12-05 03:12:00 +0700

    3h11

--- server/src/index.js
+++ server/src/index.js
@@ -19,8 +19,8 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-  exposedHeaders: ['set-cookie'],
 }
+
 app.use(cors(corsOptions))
 app.options('*', cors(corsOptions))
 app.use(cookieParser())
